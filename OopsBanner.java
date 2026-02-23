022e14dab1dcce4a87904d6322e485bf1ad1b6d0
parthvats-code <parthvats184@gmail.com>
2026-02-24 05:27:52 +0530

    t2

--- OopsBanner.java
+++ OopsBanner.java
@@ -1,6 +1,7 @@
 
-public class OopsBanner{
+public class OopsBanner {
 	public static void main(String[] args) {
-		System.out.println("OOPS") ;
+		System.out.println("OOPS");
+		// committest
 	}
 }
